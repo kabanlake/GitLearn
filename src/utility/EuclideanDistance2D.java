b80ec530add7f307723effdb0f0ae8baf6269701
kabanlake <kfuluck@gmail.com>
2020-12-05 20:22:44 +0800

    Tom实现了欧几里得距离的计算

---
+++ src/utility/EuclideanDistance2D.java
@@ -0,0 +1,8 @@
+package utility;
+
+public class EuclideanDistance2D implements Distance2D{
+    @Override
+    public double dist(Point2D p1, Point2D p2) {
+        return Math.sqrt(Math.pow(p1.getX() - p2.getX(), 2) + Math.pow(p1.getY() - p2.getY(), 2));
+    }
+}
